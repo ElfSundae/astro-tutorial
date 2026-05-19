18f0b83e72973549edc06489e1695be55e9cc16a
Elf Sundae <elf.sundae@gmail.com>
2026-05-20 03:50:38 +0800

    Add Pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://elfsundae.github.io',
+  base: '/astro-tutorial',
+});
