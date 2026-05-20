169fe34fedb296cfb2797b9f8ef5de0ac704c01d
Elf Sundae <elf.sundae@gmail.com>
2026-05-20 19:07:18 +0800

    npx astro add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elfsundae.github.io',
   base: '/astro-tutorial',
   trailingSlash: "always",
+  integrations: [preact()],
 });
